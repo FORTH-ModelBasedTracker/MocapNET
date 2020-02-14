fcf573608100840b30583041f1878517e0941de7
Ammar Qammaz <ammarkov@ics.forth.gr>
2020-02-14 13:08:17 +0200

    the latest commit might cause problems for people that haven't also pulled the latest version of the RGBDAcquisition dependency, to solve this I added an update.sh script that automatically pulls everything at the same time to ensure no problems arise..

---
+++ update.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd "$DIR"
+
+
+if [ -f dependencies/RGBDAcquisition/README.md ]; then
+ git pull origin master
+cd "$DIR"
+else
+echo "Could not find RGBDAcquisition, please rerun the initialize.sh script .." 
+fi
+
+
+
+if [ -f dependencies/AmmarServer/README.md ]; then
+echo "AmmarServer appears to exist, reverting it .."
+cd dependencies/AmmarServer 
+git pull origin master
+cd "$DIR"
+fi
+
+
+#Now sync rest of code
+cd "$DIR"
+git pull origin master
+  
+
+exit 0
