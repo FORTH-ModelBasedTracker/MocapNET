f79f22967433c432f04e9932d0027ba8852ef2e2
Ammar Qammaz <ammarkov@gmail.com>
2020-10-18 22:24:16 +0300

    split into basic functions..

--- src/Tensorflow2/main.cpp
+++ src/Tensorflow2/main.cpp
@@ -1,18 +1,46 @@
 #include <stdlib.h>
-#include <stdio.h>
-#include <tensorflow/c/c_api.h>
+#include <stdio.h> 
+#include <tensorflow/c/c_api.h> // TensorFlow C API header
+#include <vector>
+
+
+/**
+ * @brief A structure that holds all of the relevant information for a tensorflow instance
+ * This is used to simplify context switching and reduce the complexity of the library
+ */
+struct Tensorflow2Instance
+{
+  char modelPath[1024];
+  char inputLayerName[512];
+  char outputLayerName[512];
+
+  TF_Graph* graph;
+  TF_Session* session;
+  unsigned int inputElements;
+  TF_Tensor*  inputTensor;
+  TF_Tensor*  outputTensor;
+  unsigned int outputElements;
+ 
+  TF_Output* input;
+  TF_Output t0;
+  
+  int numberOfInputTensors;
+  int numberOfOutputTensors;
+ 
+  TF_Tensor** inputValues;
+  TF_Tensor** outputValues;
+  
+  
+  TF_Output* output;
+  TF_Output t2;
+ 
+  TF_Status* status;
+  TF_SessionOptions* sessionOptions;
+};
 
 void NoOpDeallocator(void* data, size_t a, void* b) {}
 
-int main()
-{
-    //********* Read model
-    TF_Graph* Graph = TF_NewGraph();
-    TF_Status* Status = TF_NewStatus();
-
-    TF_SessionOptions* SessionOpts = TF_NewSessionOptions();
-    TF_Buffer* RunOpts = NULL;
-    //saved_model_cli show --dir tmp.pb --tag_set serve --signature_def serving_default
+//saved_model_cli show --dir tmp.pb --tag_set serve --signature_def serving_default
 /*
 The given SavedModel SignatureDef contains the following input(s):
   inputs['input_front'] tensor_info:
@@ -26,62 +54,87 @@ The given SavedModel SignatureDef contains the following output(s):
       name: StatefulPartitionedCall:0
 Method name is: tensorflow/serving/predict
 */
-    const char* saved_model_dir = "/home/ammar/Documents/Programming/DNNTracker/tensorflow2GPU/src/tmp.pb/";
+
+
+int tf2_loadModel(struct Tensorflow2Instance * tf2i,const char * path,unsigned int inputDimension,unsigned int outputDimension)
+{
+    //Tensorflow boilerplate names, fingers crossed that they don't change them..
+    char inputTensorName[]={"serving_default_input_front"}; //Default input layer
+    char outputTensorName[]={"StatefulPartitionedCall"};    //Default output layer 
     const char* tags = "serve"; // default model serving tag; can change in future
     int ntags = 1;
+    
+    //Allocate stuff..
+    tf2i->graph = TF_NewGraph();
+    tf2i->status = TF_NewStatus();
+    tf2i->sessionOptions = TF_NewSessionOptions();
+    TF_Buffer* runOptions = NULL;
 
-    TF_Session* Session = TF_LoadSessionFromSavedModel(SessionOpts, RunOpts, saved_model_dir, &tags, ntags, Graph, NULL, Status);
-    if(TF_GetCode(Status) == TF_OK)
+    //Actual loading from a saved model assuming :
+    //https://www.tensorflow.org/api_docs/python/tf/saved_model/save
+    tf2i->session = TF_LoadSessionFromSavedModel(
+                                                       tf2i->sessionOptions,
+                                                       runOptions,
+                                                       path,
+                                                       &tags,
+                                                       ntags,
+                                                       tf2i->graph,
+                                                       NULL,
+                                                       tf2i->status
+                                                      );
+    if(TF_GetCode(tf2i->status) != TF_OK) 
     {
-        printf("TF_LoadSessionFromSavedModel OK\n");
-    }
-    else
-    {
-        printf("%s",TF_Message(Status));
+        fprintf(stderr,"Error loading model : %s \n",TF_Message(tf2i->status));
+        return 0;
     }
     
-    char inputTensor[]={"serving_default_input_front"};
-    char outputTensor[]={"StatefulPartitionedCall"};
 
-    //****** Get input tensor
-    //TODO : need to use saved_model_cli to read saved_model arch
-    int NumInputs = 1;
-    TF_Output* Input = (TF_Output*)malloc(sizeof(TF_Output) * NumInputs);
-
-    TF_Output t0 = {TF_GraphOperationByName(Graph,inputTensor), 0};
-    if(t0.oper == NULL)
-        printf("ERROR: Failed TF_GraphOperationByName %s\n",inputTensor);
-    else
-	printf("TF_GraphOperationByName %s is OK\n",inputTensor);
+    //****** Get input tensor 
+    tf2i->numberOfInputTensors = 1;
+    tf2i->input = (TF_Output*)malloc(sizeof(TF_Output) * tf2i->numberOfInputTensors); 
+    tf2i->t0 = {TF_GraphOperationByName(tf2i->graph,inputTensorName), 0};
+    if(tf2i->t0.oper == NULL)
+        { 
+          fprintf(stderr,"ERROR: Failed TF_GraphOperationByName %s\n",inputTensorName); 
+          return 0;
+        }
     
-    Input[0] = t0;
+    tf2i->input[0] = tf2i->t0;
     
     //********* Get Output tensor
-    int NumOutputs = 1;
-    TF_Output* Output = (TF_Output*)malloc(sizeof(TF_Output) * NumOutputs);
+    tf2i->numberOfOutputTensors = 1;
+    tf2i->output = (TF_Output*)malloc(sizeof(TF_Output) * tf2i->numberOfOutputTensors);
 
-    TF_Output t2 = {TF_GraphOperationByName(Graph,outputTensor), 0};
-    if(t2.oper == NULL)
-        printf("ERROR: Failed TF_GraphOperationByName %s\n",outputTensor);
+    tf2i->t2 = {TF_GraphOperationByName(tf2i->graph,outputTensorName), 0};
+    if(tf2i->t2.oper == NULL)
+        printf("ERROR: Failed TF_GraphOperationByName %s\n",outputTensorName);
     else	
-	printf("TF_GraphOperationByName %s is OK\n",outputTensor);
+	printf("TF_GraphOperationByName %s is OK\n",outputTensorName);
     
-    Output[0] = t2;
+    tf2i->output[0] = tf2i->t2;
 
     //********* Allocate data for inputs & outputs
-    TF_Tensor** InputValues  = (TF_Tensor**) malloc(sizeof(TF_Tensor*)*NumInputs);
-    TF_Tensor** OutputValues = (TF_Tensor**) malloc(sizeof(TF_Tensor*)*NumOutputs);
+    tf2i->inputValues  = (TF_Tensor**) malloc(sizeof(TF_Tensor*)*tf2i->numberOfInputTensors);
+    tf2i->outputValues = (TF_Tensor**) malloc(sizeof(TF_Tensor*)*tf2i->numberOfOutputTensors);
+    return 1;
+}
 
-    int ndims = 2;
-    int64_t dims[] = {1,322};
-    float data[1*322] ;//= {1,1,1,1,1,1,1,1,1,1};
-    for(int i=0; i< (1*322); i++)
-    {
-        data[i] = 1.00;
-    }
-    int ndata = sizeof(float)*1*322 ;// This is tricky, it number of bytes not number of element
 
-    TF_Tensor* int_tensor = TF_NewTensor(TF_FLOAT, dims, ndims, data, ndata, &NoOpDeallocator, 0);
+void tf2_unloadModel(struct Tensorflow2Instance * tf2i)
+{
+    // //Free memory
+    TF_DeleteGraph(tf2i->graph);
+    TF_DeleteSession(tf2i->session, tf2i->status);
+    TF_DeleteSessionOptions(tf2i->sessionOptions);
+    TF_DeleteStatus(tf2i->status); 
+}
+
+
+
+int tf2_run(struct Tensorflow2Instance * tf2i,int64_t * dimensions,unsigned int dimensionsNumber,float * data,unsigned int dataSize)
+{
+    
+    TF_Tensor* int_tensor = TF_NewTensor(TF_FLOAT, dimensions, dimensionsNumber, data, dataSize, &NoOpDeallocator, 0);
     if (int_tensor != NULL)
     {
         printf("TF_NewTensor is OK\n");
@@ -89,35 +142,65 @@ Method name is: tensorflow/serving/predict
     else
 	printf("ERROR: Failed TF_NewTensor\n");
     
-    InputValues[0] = int_tensor;
+    tf2i->inputValues[0] = int_tensor;
     
-    // //Run the Session
-    TF_SessionRun(Session, NULL, Input, InputValues, NumInputs, Output, OutputValues, NumOutputs, NULL, 0,NULL , Status);
+    //Run the Session
+    TF_SessionRun(
+                   tf2i->session,
+                   NULL,
+                   tf2i->input,
+                   tf2i->inputValues,
+                   tf2i->numberOfInputTensors,
+                   tf2i->output,
+                   tf2i->outputValues,
+                   tf2i->numberOfOutputTensors, 
+                   NULL, 
+                   0,
+                   NULL,
+                   tf2i->status
+                 );
 
-    if(TF_GetCode(Status) == TF_OK)
+    if(TF_GetCode(tf2i->status) == TF_OK)
     {
         printf("Session is OK\n");
+        return 1;
     }
     else
     {
-        printf("%s",TF_Message(Status));
+        printf("%s",TF_Message(tf2i->status));
     }
-
-    // //Free memory
-    TF_DeleteGraph(Graph);
-    TF_DeleteSession(Session, Status);
-    TF_DeleteSessionOptions(SessionOpts);
-    TF_DeleteStatus(Status);
+  return 0;
+}
 
 
-    void* buff = TF_TensorData(OutputValues[0]);
+int main()
+{
+    const char* savedModelDirectory = "/home/ammar/Documents/Programming/DNNTracker/tensorflow2GPU/src/tmp.pb/";
+    struct Tensorflow2Instance upperbodyFront={0};
+    tf2_loadModel(&upperbodyFront,savedModelDirectory,322,42);
+    
+    int ndims = 2;
+    int64_t dims[] = {1,322};
+    
+    float data[1*322] ;//= {1,1,1,1,1,1,1,1,1,1};
+    for(int i=0; i< (1*322); i++)
+    {
+        data[i] = 1.00;
+    }
+    int ndata = sizeof(float)*1*322 ;// This is tricky, it number of bytes not number of element
+
+    tf2_run(&upperbodyFront,dims,ndims,data,ndata);
+
+    void* buff = TF_TensorData(upperbodyFront.outputValues[0]);
     float* offsets = (float*)buff;
     printf("Result Tensor :\n");
-    for(int i=0;i<42;i++)
+    for(int i=0;i<upperbodyFront.outputElements;i++)
     {
         if(i>0) { printf(","); } 
         printf("%f",offsets[i]);
-    }
-    
+    } 
     printf("\n");
+    
+    
+    tf2_unloadModel(&upperbodyFront);
 }
