f8eb95a9e0e46232313c24310483cb90baab2b14
Ammar Qammaz <ammarkov@gmail.com>
2021-06-28 12:16:03 +0300

    fixed https://github.com/FORTH-ModelBasedTracker/MocapNET/issues/57 thanks @yangjituan

--- src/MocapNET2/MocapNETLib2/IO/skeletonAbstraction.cpp
+++ src/MocapNET2/MocapNETLib2/IO/skeletonAbstraction.cpp
@@ -53,26 +53,37 @@ int strcasecmp_sk(const char * input1,const char * input2)
 }
 
 
+
 int areFeetMissing(struct skeletonSerialized * skel)
 {
-    unsigned int activeJoints=0;
+    unsigned int activeLegJoints=0;
     for (unsigned int i=0; i<skel->skeletonBodyElements; i++)
     {
-                if ( strcasecmp_sk(skel->skeletonHeader[i].str,"visible_rfoot")!=0)  { if (skel->skeletonBody[i].value) { ++activeJoints; }  }   
-                if ( strcasecmp_sk(skel->skeletonHeader[i].str,"visible_rknee")!=0)  { if (skel->skeletonBody[i].value) { ++activeJoints; }  }   
-                if ( strcasecmp_sk(skel->skeletonHeader[i].str,"visible_lfoot")!=0)  { if (skel->skeletonBody[i].value) { ++activeJoints; }  }   
-                if ( strcasecmp_sk(skel->skeletonHeader[i].str,"visible_lknee")!=0)  { if (skel->skeletonBody[i].value) { ++activeJoints; }  } 
+        //If the specific label exists in our skeletonSerialized
+        if  (skel->skeletonHeader[i].str!=0)  
+         {
+            //The strcmp was flipped, thx to yangjituan for noticing this https://github.com/FORTH-ModelBasedTracker/MocapNET/issues/57 
+            //This is a pretty inefficient function, at some point I need to restructure the skeletonSerialized structure to an enum like 
+            //https://github.com/FORTH-ModelBasedTracker/MocapNET/blob/master/src/MocapNET2/MocapNETLib2/mocapnet2.hpp#L839
+            //it would just be 4 floating point checks instead of this loop going through labels etc.
+ 
+            //If we are at the correct skeletonSerialized label for the particular r/l foot or knee and check its value and it is non-zero
+            //we can count it as present in the list of activeLegJoints count the number of active joints.. 
+            if ( (strcasecmp_sk(skel->skeletonHeader[i].str,"visible_rfoot")==0) && (skel->skeletonBody[i].value) )  { ++activeLegJoints; } else
+            if ( (strcasecmp_sk(skel->skeletonHeader[i].str,"visible_rknee")==0) && (skel->skeletonBody[i].value) )  { ++activeLegJoints; } else
+            if ( (strcasecmp_sk(skel->skeletonHeader[i].str,"visible_lfoot")==0) && (skel->skeletonBody[i].value) )  { ++activeLegJoints; } else
+            if ( (strcasecmp_sk(skel->skeletonHeader[i].str,"visible_lknee")==0) && (skel->skeletonBody[i].value) )  { ++activeLegJoints; }
+         }
     }
     
-    if (activeJoints<3) 
+    if (activeLegJoints<3) 
     {
- 
-      fprintf(stderr,YELLOW "Feet are missing, Only %u leg joints detected..\n" NORMAL,activeJoints);
+      fprintf(stderr,YELLOW "Feet are missing, Only %u leg joints detected..\n" NORMAL,activeLegJoints);
       return 1; 
     }
-    
     return 0;
 }
+
 
 
 
