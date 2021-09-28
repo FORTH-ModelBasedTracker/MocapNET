43d4ec10d9d9865a65f6017b3242e7a2a97cf26c
Ammar Qammaz <ammarkov@gmail.com>
2021-09-28 11:20:52 +0300

    changed the opencv script to use the github repo and easily change versions..

--- scripts/getOpenCV3.2.sh
+++ scripts/getOpenCV3.2.sh
@@ -11,22 +11,31 @@ sudo apt-get install build-essential cmake git libgtk2.0-dev pkg-config ffmpeg l
 #cd "$1"
 echo "Will Install @ `pwd`"
 
+
+#Want a different version?
+#Check https://opencv.org/releases/ for versions and 
+#change the OPENCV_VERSION in the next line..
+OPENCV_VERSION="3.2.0"
 echo "Downloading"
 
-wget http://ammar.gr/programs/opencv-3.2.0.zip
-wget http://ammar.gr/programs/opencv_contrib-3.2.0.tar.gz
+wget http://ammar.gr/programs/opencv-$OPENCV_VERSION.zip
+wget http://ammar.gr/programs/opencv_contrib-$OPENCV_VERSION.tar.gz
+
+#wget https://codeload.github.com/opencv/opencv/zip/$OPENCV_VERSION -O opencv-$OPENCV_VERSION.zip
+#wget https://codeload.github.com/opencv/opencv_contrib/zip/$OPENCV_VERSION -O opencv_contrib-$OPENCV_VERSION.tar.gz
 
 echo "Extracting"
 
-tar xvzf opencv_contrib-3.2.0.tar.gz
-unzip opencv-3.2.0.zip
+#tar xvzf opencv_contrib-$OPENCV_VERSION.tar.gz
+unzip opencv_contrib-$OPENCV_VERSION.zip
+unzip opencv-$OPENCV_VERSION.zip
 
 echo "Building"
 
-cd opencv-3.2.0
+cd opencv-$OPENCV_VERSION
 mkdir build
 cd build
-cmake -DOPENCV_ENABLE_NONFREE=ON -DOPENCV_EXTRA_MODULES_PATH=$DEPENDENCIES_PATH/opencv_contrib-3.2.0/modules ..
+cmake -DOPENCV_ENABLE_NONFREE=ON -DOPENCV_EXTRA_MODULES_PATH=$DEPENDENCIES_PATH/opencv_contrib-$OPENCV_VERSION/modules ..
 make -j5
 
 echo "Do you want to install OpenCV to your system ? " 
