7620e1708b885bc0ec28c87bafc873502d56da4d
Ammar Qammaz <ammarkov@gmail.com>
2023-09-13 11:01:37 +0300

    ...

--- src/python/mnet4/setup.sh
+++ src/python/mnet4/setup.sh
@@ -4,7 +4,7 @@ cd "$DIR"
 
 #Simple dependency checker that will apt-get stuff if something is missing
 # sudo apt-get install python3-venv python3-pip
-SYSTEM_DEPENDENCIES="python3-venv python3-pip"
+SYSTEM_DEPENDENCIES="build-essential python3-venv python3-pip"
 
 for REQUIRED_PKG in $SYSTEM_DEPENDENCIES
 do
@@ -25,10 +25,38 @@ done
 #------------------------------------------------------------------------------
 
  #ONNX needs protobuf AI need avdevice ?
- sudo apt-get install protobuf-compiler libprotobuf-dev libavdevice-dev
+sudo apt-get install protobuf-compiler libprotobuf-dev libavdevice-dev
  
+
+if [[ $* == *--collab* ]]
+then 
+ echo "Collab mode!"
+ echo "Performing python package installation without a virtual environment"
+else 
+ echo "Using a python pythonVirtualEnvironment environment"
+ echo "Activate it using : source pythonVirtualEnvironment/bin/activate"
  python3 -m venv pythonVirtualEnvironment
  source pythonVirtualEnvironment/bin/activate
+fi
+
+
+
+cd "$DIR"
+if [ -f RGBDAcquisition/README.md ]; then
+echo "RGBDAcquisition appears to already exist .."
+else
+ git clone https://github.com/AmmarkoV/RGBDAcquisition
+ cd RGBDAcquisition
+ mkdir build
+ cd build
+ cmake ..
+ make BVHTester
+ cd "$DIR"
+ ln -s RGBDAcquisition/opengl_acquisition_shared_library/opengl_depth_and_color_renderer/src/Applications/BVHTester BVH
+ ln -s RGBDAcquisition/opengl_acquisition_shared_library/opengl_depth_and_color_renderer/BVHTester GroundTruthDumper
+fi
+
+
  
  python3 -m pip install --upgrade pip #2.8.0 nvidia-tensorrt
  python3 -m pip install tensorflow numpy numba tensorboard_plugin_profile tensorflow-model-optimization keras pillow tf2onnx onnxruntime onnx matplotlib pydot mediapipe opencv-python scikit-learn
@@ -38,6 +66,9 @@ done
  #Get Models!
  python3 -m getModelFromDatabase.py --get 310 
 
+
+ #Doing a test run!
+
  echo "To enable Tensorflow profiling for non-root users please add : "
  echo "options nvidia \"NVreg_RestrictProfilingToAdminUsers=0\""
  echo "to /etc/modprobe.d/nvidia-graphics-drivers.conf"
@@ -45,11 +76,13 @@ done
  echo "Ready for use"
  echo "From now on to work with MocapNET you can use :  "
  echo "source pythonVirtualEnvironment/bin/activate"
- echo "!python3 -m mediapipeHolisticWebcamMocapNET --from webcam"
+ echo "!python3 -m mediapipeHolisticWebcamMocapNET --from webcam  --save --plot"
  echo " OR "
- echo "!python3 -m mediapipeHolisticWebcamMocapNET --from shuffle.webm"
+ echo "!python3 -m mediapipeHolisticWebcamMocapNET --from shuffle.webm --save --plot"
  echo " OR "
- echo "!python3 -m csvNET --from ../../../dataset/sample.csv"
+ echo "!python3 -m csvNET --from ../../../dataset/sample.csv  --save --plot"
+ echo "  "
+ echo " after the run is complete out.bvh will contain your results  "
 
 
 
