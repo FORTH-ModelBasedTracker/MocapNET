cb2e2bb17a94a560009bb6c689e2d41ae29749d5
Ammar Qammaz <ammarkov@ics.forth.gr>
2023-10-13 13:42:46 +0300

    downgrade to stable 314 model

--- src/python/mnet4/setup.sh
+++ src/python/mnet4/setup.sh
@@ -82,8 +82,8 @@ fi
  #   zip mnet4_318.zip step1_*/*
 
  #Another just take the precompiled ensembles ready to run
- wget http://ammar.gr/mocapnet/mnet4/mnet4_318.zip -O mnet4_318.zip
- unzip -o mnet4_324.zip #<- upgrade to 324 mnet4_318.zip
+ wget http://ammar.gr/mocapnet/mnet4/mnet4_314.zip -O mnet4_314.zip
+ unzip -o mnet4_314.zip #<- upgrade to 324 mnet4_318.zip
 
  #Unzip models for PoseNET.py operation..
  wget http://ammar.gr/mocapnet/mnet4/mediapipe.zip -O mediapipe.zip
